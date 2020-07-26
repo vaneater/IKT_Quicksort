28984512080506fd31ef7e551e0a98e89f573632
vanessa <vanessagatner@gmail.com>
2020-07-27 01:25:59 +0200

    rest des codes von array zu array liste geändert.

--- src/Quicksort_Vanessa.java
+++ src/Quicksort_Vanessa.java
@@ -8,6 +8,7 @@
     das original von herr witt ist .class
 
     hat das einen einfluss auf den code? ich babe den originalen code kopiert, und dieser funktioniert nämlich nicht
+    -> index out of bound exception 
 
     ich hab erst mal versucht das einlesen der datei schneller zu machen
     dazu statt einem array eine liste benutzen, da diese angeblich schneller sind lol
@@ -25,49 +26,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Quicksort_Vanessa extends ProfiledClass {
-    ArrayList<Integer> field;
-    final int outputIndex;
+    ArrayList<Integer> field; // Liste ist schneller als Array
+    final int outputIndex;  //
+    ArrayList<Integer> checks; // liste aus der run methode
 
     public Quicksort_Vanessa(String inputFile, Integer outputIndex) throws IOException {
         this.outputIndex = outputIndex;
 
-        File file = new File(inputFile);
-        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        File file = new File(inputFile); //File wird in 'file' gepspeichert
+        DataInputStream in = new DataInputStream(new FileInputStream(file)); //File wird gelesen und in 'in' gespeichert
 
-        for(int index = 0; index < this.field.size(); ++index) {
-            this.field.add(index, in.readInt());
+        for(int index = 0; index < this.field.size(); ++index) { //wir lesen jeden INT in unserer liste 'field' und füht es in 'in' ein
+            this.field.add(index, in.readInt()); //wir speichern den Index also stelle der zu speichernden Zahl und die Zahl selber.
+            // Bei einem normalen array ist dieser Schritt nicht notwendig da man bei array direkt über den index zugreifen kann
         }
 
-        in.close();
+        in.close(); //wir schliessen die Datei 'in'
 
     }
 
-    static void quicksort(List<Integer> a, int lo, int hi) {
+    static void quicksort(List<Integer> a, int lo, int hi) { //lo=linke seite und hi = rechte seite
         if (lo < hi) {
-            int pivotIndex = partition(a, lo, hi);
-            quicksort(a, lo, pivotIndex);
-            quicksort(a, pivotIndex + 1, hi);
+            int pivotIndex = partition(a, lo, hi); //wir teilen unsere Liste in 2 Hälften
+            quicksort(a, lo, pivotIndex); //und rekursion angewendet auf die linke Seite
+            quicksort(a, pivotIndex + 1, hi); //und rekursion auf rechter seite
         }
 
     }
 
     static int partition(List<Integer> a, int lo, int hi) {
-        int pivot = a.get((hi + lo) / 2);
+        int pivot = a.get((hi + lo) / 2); //pivot ist unsere Mitte(index)
         int i = lo;
         int j = hi;
 
-        while(true) {
-            while(a.get(i) >= pivot) {
-                while(a.get(j) > pivot) {
-                    --j;
+        while(true) { //wann ist false und wann true?
+            while(a.get(i) >= pivot) { // wir vergleichen linke seite mit pivot/mitte
+                while(a.get(j) > pivot) { //wenn rechte seite größer ist als pivot
+                    --j; //dann rückt rechte seite eine stelle bzw index nach links
                 }
 
-                if (i >= j) {
-                    return j;
+                if (i >= j) { //überprüft ob linke seite größer gleich ist als rechte seite
+                    return j; //warum return j?
                 }
 
-                int tmp = a.get(i);
-                a.set(i, j);
+                int tmp = a.get(i); //warum tauschen wir hier linke mit rechter seite? warum wird i in tmp gespeichert
+                a.set(i, j);   // set(index, die zahlt selber)
                 a.set(j, tmp);
                 ++i;
                 --j;
@@ -79,12 +82,17 @@ public class Quicksort_Vanessa extends ProfiledClass {
 
     public void run() {
         quicksort(this.field, 0, this.field.size() - 1);
-        int[] checks = new int[]{0, 5000, 600000, this.field.size() - 1};
-        int[] var2 = checks;
-        int var3 = checks.length;
+        //woher kommen die zahlen 5000 und 600.000?
+        // alle seltsamen zahlen einzeln hinzufügen
+        checks.add(0);
+        checks.add(5000);
+        checks.add(600000);
+        checks.add(this.field.size() -1);
+        ArrayList<Integer> var2 = checks; //dasselbe array unter einem anderen namen kopiert
+        int var3 = checks.size(); //varibale die die Länge des array speichert
 
         for(int var4 = 0; var4 < var3; ++var4) {
-            int check = var2[var4];
+            int check = var2.get(var4);
             System.out.println("Element an Index " + check + ": " + this.field.get(check));
         }
 
@@ -94,7 +102,7 @@ public class Quicksort_Vanessa extends ProfiledClass {
     public static void main(String[] arguments) {
         //Profiler profiler = new Profiler(Quicksort_Vanessa.class, new Object(), Integer.parseInt(arguments[1]));
         Profiler profiler = new Profiler(Quicksort_Vanessa.class, new Object[]{arguments[0], Integer.parseInt(arguments[1])});
-        profiler.start();
+        profiler.start(); //profiler für Laufzeit rechnung
         profiler.printResults();
     }
 }
